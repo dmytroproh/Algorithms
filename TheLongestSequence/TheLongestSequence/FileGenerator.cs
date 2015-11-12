7f80819181878bba2355bb3d7201ac5acd89cbc5
dmytroproh <dmytroproh@gmail.com>
2015-11-13 00:53:53 +0200

    Start

---
+++ TheLongestSequence/TheLongestSequence/FileGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TheLongestSequence
+{
+   static class FileGenerator
+    {
+       public static string Create() {
+         
+           var input = new FileInfo("input.txt");         
+           StreamWriter writer = input.CreateText();
+           Random rand = new Random();
+           for (int i = 0; i < rand.Next(50,100); i++)
+           {
+               writer.Write((byte)rand.Next(0, 2));  
+           }
+           writer.Close();
+          
+           return input.FullName;
+       }
+       public static void Save(int longestValue)
+       {
+           FileInfo output = new FileInfo("output.txt");
+           StreamWriter writer = output.CreateText();
+           writer.Write(longestValue);
+           writer.Close();
+          
+       }
+    }
+}
---
+++ TheLongestSequence/TheLongestSequence/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TheLongestSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<byte> list = new List<byte>();        
+            string path = FileGenerator.Create();
+            string byteStryng = File.ReadAllText(path);   
+    
+            foreach (char symbol in byteStryng)   
+               list.Add((byte)Char.GetNumericValue(symbol));
+            
+            int logestSequenceValue = FindLongest(list);
+            FileGenerator.Save(logestSequenceValue);
+
+        }
+
+        private static int FindLongest(List<byte> list)
+        {
+            int result = 0;
+            int maxValue = 0;
+            foreach (byte arr in list)
+            {
+                result = result * arr + arr;
+                if (result > maxValue)
+                    maxValue = result;
+
+            }
+            return maxValue;
+        }
+    }
+}
